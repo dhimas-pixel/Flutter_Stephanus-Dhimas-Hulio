975c9bb0a47b16f8190f4c00d9bb7adc72c8156e
Dhimas_Mbuh <76719135+dhimas-pixel@users.noreply.github.com>
2022-03-09 14:24:30 +0700

    Update main.dart

--- 9_Dart Object Oriented Programming 1/Praktikum/main.dart
+++ 9_Dart Object Oriented Programming 1/Praktikum/main.dart
@@ -3,47 +3,63 @@ import 'dart:io';
 import 'package:dart_oop1/dart_oop1.dart' as dart_oop1;
 
 void main(List<String> arguments) {
-  var input, maks = 5;
+  var input;
   Hewan hewan = Hewan();
   Mobil mobil = Mobil();
   while (input != "n") {
-    if (mobil.totalMuatan() < maks) {
-      // List Berat Hewan
-      for (var item in hewan.berat.keys) {
-        print("Hewan: $item dengan Berat ${hewan.berat[item]} Kg");
-      }
+    if (mobil.totalMuatan() < mobil.kapasitas) {
       print("-------------------------------------------");
+
       stdout.write("Masukkan Nama Hewan : ");
       var nama = stdin.readLineSync();
-      mobil.tambahMuatan(nama!);
-      print("\nTotal Muatan : " + mobil.totalMuatan().toString());
+
+      stdout.write("Masukkan Berat Hewan : ");
+      int berat = int.parse(stdin.readLineSync()!);
+      mobil.tambahMuatan(nama!, berat);
+
+      print("\nTotal Muatan " +
+          mobil.muatan.length.toString() +
+          " dengan berat " +
+          mobil.totalMuatan().toString() +
+          " Kg");
+
       stdout.write("\nMau Input muatan lagi? [y/n] => ");
       input = stdin.readLineSync();
     } else {
       print("\nMuatan sudah mencapai maksimal!");
-      print("Total Muatan : " + mobil.totalMuatan().toString());
       break;
     }
   }
-  print(mobil.muatan.toString());
+  print("\nTotal Muatan " +
+      mobil.muatan.length.toString() +
+      " dengan berat " +
+      mobil.totalMuatan().toString() +
+      " Kg");
 }
 
 // Class Hewan
 class Hewan {
-  Map berat = {"Ayam": 5, "Sapi": 30, "Kambing": 15, "Beruang": 50};
+  List<int> berat = [];
 }
 
 // Class Mobil
 class Mobil {
+  Hewan hewan = Hewan();
   List<dynamic> muatan = [];
+  int kapasitas = 100;
 
 // Method tambah muatan
-  void tambahMuatan(String tambah) {
+  void tambahMuatan(String tambah, int berat) {
     muatan.add(tambah);
+    hewan.berat.add(berat);
   }
 
 // method total muatan
   int totalMuatan() {
-    return muatan.length;
+    var total = 0;
+    for (var item in hewan.berat) {
+      total += item;
+    }
+    return total;
   }
 }
