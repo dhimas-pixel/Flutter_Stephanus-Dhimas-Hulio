b9c47733cfda37e04b7da7daae7d46dd47f8b973
Dhimas_Mbuh <76719135+dhimas-pixel@users.noreply.github.com>
2022-03-18 15:53:21 +0700

    Create chart_model.dart

---
+++ 12_Platform Widget/Praktikum/material/chart_model.dart
@@ -0,0 +1,75 @@
+class ChartModel {
+  final String name;
+  final String message;
+  final String time;
+  final String profileUrl;
+
+  ChartModel(
+      {required this.name,
+      required this.message,
+      required this.time,
+      required this.profileUrl});
+}
+
+final List<ChartModel> items = [
+  ChartModel(
+      name: 'Aimer',
+      message: 'Daisuki',
+      time: '13.00',
+      profileUrl:
+          'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/905.jpg'),
+  ChartModel(
+      name: 'Marcelo',
+      message: 'Lagi Apa?',
+      time: '12.13',
+      profileUrl:
+          'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/447.jpg'),
+  ChartModel(
+      name: 'CR7',
+      message: 'SIIIIIIIIIII!!!!',
+      time: '01.00',
+      profileUrl:
+          'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/635.jpg'),
+  ChartModel(
+      name: 'Marco',
+      message: 'Ginjalmu ada 10',
+      time: '12.00',
+      profileUrl:
+          'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/391.jpg'),
+  ChartModel(
+      name: 'Hayam Wuruk',
+      message: 'Istriku Selingkuh',
+      time: '20.45',
+      profileUrl:
+          'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/122.jpg'),
+  ChartModel(
+      name: 'Sintia',
+      message: 'Halo!',
+      time: '13.00',
+      profileUrl:
+          'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1021.jpg'),
+  ChartModel(
+      name: 'Lisa',
+      message: 'Selamat Datang di Indomaret!',
+      time: '13.13',
+      profileUrl:
+          'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/78.jpg'),
+  ChartModel(
+      name: 'Julio Cesar',
+      message: 'Selamat Belanja!',
+      time: '07.15',
+      profileUrl:
+          'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/460.jpg'),
+  ChartModel(
+      name: 'Wahyu',
+      message: 'Woi Jomblo',
+      time: '00.00',
+      profileUrl:
+          'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/127.jpg'),
+  ChartModel(
+      name: 'Bagas',
+      message: 'Sok Asik deh',
+      time: '09.56',
+      profileUrl:
+          'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/881.jpg'),
+];
