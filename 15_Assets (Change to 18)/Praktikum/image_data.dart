b316b35447121122a048695074d6cf3b2ffd058e
Dhimas_Mbuh <76719135+dhimas-pixel@users.noreply.github.com>
2022-03-25 16:12:32 +0700

    Add files via upload

---
+++ 15_Assets (Change to 18)/Praktikum/image_data.dart
@@ -0,0 +1,11 @@
+class ImageData {
+  final String gambar;
+
+  ImageData({required this.gambar});
+}
+
+List<ImageData> getImage = [
+  ImageData(gambar: "https://picsum.photos/id/10/200/300"),
+  ImageData(gambar: "https://picsum.photos/id/1004/200/300"),
+  ImageData(gambar: "https://picsum.photos/id/1005/200/300"),
+];
