104ac8e579e0365c3d60403964c8098aaa8e3f6a
Dhimas_Mbuh <76719135+dhimas-pixel@users.noreply.github.com>
2022-04-08 11:17:24 +0700

    Delete 21_Flutter State Management (BLoC)/Prakrikum directory

--- 21_Flutter State Management (BLoC)/Prakrikum/bloc/counter_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'counter_bloc.dart';
-
-class CounterState extends Equatable {
-  List<ContactsModel> contacts = [];
-  CounterState({required this.contacts});
-
-  @override
-  List<Object> get props => [contacts];
-}
--- 21_Flutter State Management (BLoC)/Prakrikum/contacts_model.dart
+++
@@ -1,9 +0,0 @@
-class ContactsModel {
-  final String name;
-  final String number;
-
-  ContactsModel({
-    required this.name,
-    required this.number,
-  });
-}
