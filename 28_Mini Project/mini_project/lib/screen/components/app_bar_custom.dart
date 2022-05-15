03da2af9fbb4cdc31eda9b7a25e22d144ddf61d9
ephanzen@gmail.com <ephanzen4@gmail.com>
2022-05-15 20:20:05 +0700

    Mini Project

---
+++ 28_Mini Project/mini_project/lib/screen/components/app_bar_custom.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AppBarCustom extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final String page;
+
+  const AppBarCustom({
+    Key? key,
+    required this.title,
+    required this.page,
+  }) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(55);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        title,
+        style: const TextStyle(color: Colors.black),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      leading: IconButton(
+        onPressed: () {
+          Navigator.pushReplacementNamed(context, page);
+        },
+        icon: const Icon(
+          Icons.arrow_back,
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
